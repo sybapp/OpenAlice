395302b3b07cd0d0dee295edf61785db1ab2f9b4
sybapp <sybapp@qq.com>
2026-06-07 22:10:05 +0800

    feat(market-data): add generic provider-transparent service

---
+++ src/services/market-data/index.ts
@@ -0,0 +1,17 @@
+export {
+  MARKET_DATA_DEFAULT_LIMIT,
+  MARKET_DATA_MAX_LIMIT,
+  type MarketDataAssetClass,
+  type MarketDataCatalog,
+  type MarketDataCatalogEndpoint,
+  type MarketDataCatalogProvider,
+  type MarketDataConfig,
+  type MarketDataEnvelope,
+  type MarketDataHistoricalInput,
+  type MarketDataQueryInput,
+  type MarketDataScanInput,
+  type MarketDataScanPreset,
+  type MarketDataSearchInput,
+  type MarketDataServiceDeps,
+} from './types.js'
+export { MarketDataService, createMarketDataService } from './service.js'
